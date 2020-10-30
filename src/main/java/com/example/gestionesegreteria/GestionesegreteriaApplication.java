27d782fe7746c4dc1c45c246da027b3faae19ad9
lovaion <67899461+lovaion@users.noreply.github.com>
2020-10-30 09:00:07 +0100

    First Commit

---
+++ src/main/java/com/example/gestionesegreteria/GestionesegreteriaApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionesegreteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionesegreteriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionesegreteriaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gestionesegreteria/GestionesegreteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionesegreteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionesegreteriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
